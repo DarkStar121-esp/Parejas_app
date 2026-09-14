dad2d4fdc1d7cf6f57ec444b86c5935dbda497cc
DarkStar121-esp <villarruelsantyago@gmail.com>
2026-09-14 15:42:54 -0300

    Fix: agregar enum GameCategory y getters de compatibilidad

--- lib/core/game_module.dart
+++ lib/core/game_module.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 
+enum GameCategory { classic, couples }
+
 abstract class GameModule {
   String get id;
   String get title;
   String get description;
   IconData get icon;
   bool get isCoupleGame;
-  bool get isCompetitive; // Indicador de juego competitivo (ej: UNO, Truco)
+  bool get isCompetitive;
+
+  String get name => title;
+  bool get isAdultContent => false;
+  GameCategory get category => isCoupleGame ? GameCategory.couples : GameCategory.classic;
 
   Widget buildGameScreen(BuildContext context);
+  Widget buildScreen(BuildContext context) => buildGameScreen(context);
 }
