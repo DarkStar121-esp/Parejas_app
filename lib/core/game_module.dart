e95f28fc0071bc0774e82d2b30eca88ba909182e
DarkStar121-esp <villarruelsantyago@gmail.com>
2026-09-14 16:07:11 -0300

    Fix: proveer implementaciones por defecto en GameModule

--- lib/core/game_module.dart
+++ lib/core/game_module.dart
@@ -3,17 +3,21 @@ import 'package:flutter/material.dart';
 enum GameCategory { classic, couples }
 
 abstract class GameModule {
-  String get id;
-  String get title;
-  String get description;
-  IconData get icon;
-  bool get isCoupleGame;
-  bool get isCompetitive;
+  String get id => 'game_${title.toLowerCase().replaceAll(' ', '_')}';
+  String get title => 'Juego';
+  String get description => '';
+  IconData get icon => Icons.casino;
+  bool get isCoupleGame => false;
+  bool get isCompetitive => false;
 
+  // Propiedades de compatibilidad para HomeScreen y GameRegistry
   String get name => title;
   bool get isAdultContent => false;
   GameCategory get category => isCoupleGame ? GameCategory.couples : GameCategory.classic;
 
-  Widget buildGameScreen(BuildContext context);
+  Widget buildGameScreen(BuildContext context) => const Scaffold(
+        body: Center(child: Text('Pantalla en construcción')),
+      );
+
   Widget buildScreen(BuildContext context) => buildGameScreen(context);
 }
